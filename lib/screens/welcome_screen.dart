090339a25360def11e3b3dc6c85dc469937e1789
Abdo Tareq <abdotarekfathy@gmail.com>
2020-06-04 14:05:01 +0200

    feat: Animate logo between screens

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -22,9 +22,12 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
           children: <Widget>[
             Row(
               children: <Widget>[
-                Container(
-                  child: Image.asset('images/logo.png'),
-                  height: 60.0,
+                Hero(
+                  tag: 'logo',
+                  child: Container(
+                    child: Image.asset('images/logo.png'),
+                    height: 60.0,
+                  ),
                 ),
                 Text(
                   'Flash Chat',
@@ -43,10 +46,10 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
               child: Material(
                 elevation: 5.0,
                 color: Colors.lightBlueAccent,
+                // for oval shape
                 borderRadius: BorderRadius.circular(30.0),
                 child: MaterialButton(
                   onPressed: () {
-                    //Go to login screen.
                     Navigator.pushNamed(context, LoginScreen.id);
                   },
                   minWidth: 200.0,
@@ -65,7 +68,6 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
                 elevation: 5.0,
                 child: MaterialButton(
                   onPressed: () {
-                    //Go to registration screen.
                     Navigator.pushNamed(context, RegistrationScreen.id);
                   },
                   minWidth: 200.0,
